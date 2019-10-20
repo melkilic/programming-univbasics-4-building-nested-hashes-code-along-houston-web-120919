0df63a2e1a7516d6d567885c2589099eff58bddc
Melike KILIC <melike180400@gmail.com>
2019-10-20 03:26:52 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,15 +4,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [ {
-        :name=> "Benvolio",
+      :hero_friends => [ 
+        {:name=> "Benvolio",
         :age=> "17",
         :attitude=> "worried"
       },
       {:name=> "Mercutio",
       :age=> "18",
-      :attitude=> "hot-headed"
-        }
+      :attitude=> "hot-headed"}
         #your hashes here!
       ]
    },
@@ -20,15 +19,13 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [ {
-        :name=> "Steven",
+      :heroine_friends => [ 
+        {:name=> "Steven",
         :age=> "30",
         :attitude=> "confused"},
-        {
-          :name=> "Nurse",
+        {:name=> "Nurse",
           :age=> "44",
-          :attitude=> "worried"
-        }
+          :attitude=> "worried"}
         #your hashes here!
       ]
    }
